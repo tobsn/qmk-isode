20efcb151370f39e1724a1d5e67f812cbbde479b
Tobias Orterer <orterer@gmail.com>
2016-10-27 00:06:56 +0200

    init

---
+++ keymaps/default/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define LSPO_KEY KC_9
+#define RSPC_KEY KC_0
+
+#endif
---
+++ keymaps/default/keymap.c
@@ -0,0 +1,123 @@
+#include "isode.h"
+#include "keymap_extras/keymap_german_osx.h"
+
+#define LAYER_BASE 0
+#define LAYER_SPEC 1
+#define LAYER_RSFT 2
+#define LAYER_RGUI 3
+
+#define LSPO_KEY DE_OSX_LESS
+#define RSPC_KEY DE_OSX_MORE
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ /* BASE
+ * .-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.------.
+ * | ESC |  Q  |  W  |  E  |  R  |  T  |  Z  |  U  |  I  |  O  |  P  |  Ü  | ENTR |
+ * |-----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.-----|
+ * | TAB  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  Ö  |  Ä  |  #  |
+ * |------'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'-----|
+ * | LSHIFT  |  Y  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  -  | RSHIFT |
+ * |------.--'---.-'-----+-----'-----'----.'-----'-----'--.--'----.'-----+--------|
+ * | CTRL | LALT |  CMD  |      MOD       |     SPACE     |  CMD  | RALT |  LOCK  |
+ * `------'------'-------'----------------'---------------'-------'------'--------'
+ */
+	[LAYER_BASE] = KEYMAP( /* Base */
+		KC_ESC,   DE_OSX_Q, DE_OSX_W, DE_OSX_E, DE_OSX_R, DE_OSX_T, DE_OSX_Z, DE_OSX_U, DE_OSX_I,    DE_OSX_O,   DE_OSX_P,    DE_OSX_UE, KC_BSPACE, \
+		KC_TAB,   DE_OSX_A, DE_OSX_S, DE_OSX_D, DE_OSX_F, DE_OSX_G, DE_OSX_H, DE_OSX_J, DE_OSX_K,    DE_OSX_L,   DE_OSX_OE,   DE_OSX_AE, KC_ENTER, \
+		LSPO_KEY, DE_OSX_Y, DE_OSX_X, DE_OSX_C, DE_OSX_V, DE_OSX_B, DE_OSX_N, DE_OSX_M, DE_OSX_COMM, DE_OSX_DOT, DE_OSX_MINS, LT(LAYER_RSFT, RSPC_KEY), \
+		KC_LCTRL, KC_LALT,  KC_LGUI,            LT(LAYER_SPEC,KC_SPC),        KC_SPC,                LT(LAYER_RGUI,KC_RGUI), KC_RALT, KC_RCTRL \
+	),
+
+
+ /* BASE + MODIFIER
+ * .-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.------.
+ * |  ^  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  ß  |  +   |
+ * |-----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.-----|
+ * |      |     |     |     |     |     |     |     |     |     |     |     |     |
+ * |------'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'-----|
+ * |         |     |     |     |     |     |     |     |     |     |     |        |
+ * |------.--'---.-'-----+-----'-----'----.'-----'-----'--.--'----.'-----+--------|
+ * |      |      |       |                |               |       |      |        |
+ * `------'------'-------'----------------'---------------'-------'------'--------'
+ */
+	[LAYER_SPEC] = KEYMAP( /* Base */
+		DE_OSX_CIRC, DE_OSX_1, DE_OSX_2, DE_OSX_3, DE_OSX_4, DE_OSX_5, DE_OSX_6, DE_OSX_7, DE_OSX_8, DE_OSX_9, DE_OSX_0, DE_OSX_SS, DE_OSX_ACUT, \
+		KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, \
+		KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, \
+		KC_TRNS,     KC_TRNS,  KC_TRNS,            KC_TRNS,                      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS \
+	),
+
+ /* BASE + R_SHIFT
+ * .-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.------.
+ * |  °  |  !  |  "  |  §  |  $  |  %  |  &  |  &  |  (  |  )  |  =  |  ?  |  *   |
+ * |-----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.-----|
+ * |      |     |     |     |     |     |     |     |     |     |     |     |  '  |
+ * |------'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'-----|
+ * |         |     |     |     |     |     |     |     |     |     |     |        |
+ * |------.--'---.-'-----+-----'-----'----.'-----'-----'--.--'----.'-----+--------|
+ * |      |      |       |                |               |       |      |        |
+ * `------'------'-------'----------------'---------------'-------'------'--------'
+ */
+	[LAYER_RSFT] = KEYMAP( /* Base */
+		LSFT(DE_OSX_CIRC), LSFT(DE_OSX_1), LSFT(DE_OSX_2), LSFT(DE_OSX_3), LSFT(DE_OSX_4), LSFT(DE_OSX_5), LSFT(DE_OSX_6), LSFT(DE_OSX_7), LSFT(DE_OSX_8), LSFT(DE_OSX_9), LSFT(DE_OSX_0), DE_OSX_QST, DE_OSX_PLUS, \
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_QUOT, \
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+		KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS \
+	),
+
+
+ /* BASE + R_GUI
+ * .-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.-----.------.
+ * |     |     |     |     |     |     |     |  {  |  [  |  ]  |  }  |     |   ~  |
+ * |-----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.----'.-----|
+ * |      |     |     |     |     |     |     |     |     |     |     |     |     |
+ * |------'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'--.--'-----|
+ * |  PIPE   |     |     |     |     |     |     |     |     |     |     |        |
+ * |------.--'---.-'-----+-----'-----'----.'-----'-----'--.--'----.'-----+--------|
+ * |      |      |       |                |               |       |      |        |
+ * `------'------'-------'----------------'---------------'-------'------'--------'
+ */
+	[LAYER_RGUI] = KEYMAP( /* Base */
+		KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_LCBR, DE_OSX_LBRC, DE_OSX_RBRC, DE_OSX_RCBR, DE_OSX_BSLS, KC_TRNS, \
+		KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS, \
+		DE_OSX_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS, \
+		KC_TRNS,     KC_TRNS, KC_TRNS,            KC_TRNS,                 KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS \
+	)
+
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+	// MACRODOWN only works in this function
+			switch(id) {
+				case 0:
+					if (record->event.pressed) {
+						register_code(KC_RSFT);
+					} else {
+						unregister_code(KC_RSFT);
+					}
+				break;
+			}
+		return MACRO_NONE;
+};
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
